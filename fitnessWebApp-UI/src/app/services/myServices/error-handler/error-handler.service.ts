ac6410a3097c6b2b32915be07c638196dc055f28
Andrea Barutta <andreabarutta123@gmail.com>
2024-05-17 13:08:49 +0200

    error handling improving

--- fitnessWebApp-UI/src/app/services/myServices/error-handler/error-handler.service.ts
+++ fitnessWebApp-UI/src/app/services/myServices/error-handler/error-handler.service.ts
@@ -8,12 +8,16 @@ export class ErrorHandlerService implements ErrorHandler {
   constructor() { }
 
   handleError(error: any) {
+    let info: Array<string>  = [];
     if (error.error.validationErrors) {
-      return error.error.validationErrors;
+      info.push(error.error.validationErrors);
     }
     if (error.error.businessErrorCode >= 300) {
-      return [error.error.businessErrorDescription];
+      info.push(error.error.businessErrorDescription);
     }
+    if(!info.length) {
     return ["Qualcosa è andato storto, riprova più tardi"];
+    }
+    return info;
   }
 }
