750f5ccd38f7d75abc0c0493f7bd521a510966e9
Andrea Barutta <andreabarutta123@gmail.com>
2024-05-23 14:15:57 +0200

    resolve test context problem

--- fitnessWebApp/src/test/java/com/barutta02/FitnessApp/FitnessApplicationTests.java
+++ fitnessWebApp/src/test/java/com/barutta02/FitnessApp/FitnessApplicationTests.java
@@ -1,9 +1,17 @@
 package com.barutta02.FitnessApp;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+@RunWith(SpringRunner.class)
 
-@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(
+  locations = "classpath:application-unitTest.yml")
 class FitnessApplicationTests {
 
 	@Test
